e3648c60f81854690a8e7b9a6dd730461268ab25
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-09-24 20:35:25 +0530

    Update TCS Bubble Sort.py

--- TCS Bubble Sort.py
+++ TCS Bubble Sort.py
@@ -9,3 +9,21 @@ def Bubble_Sort(List):
     return List
 List=[64, 34, 25,90, 12, 22, 11]
 print(Bubble_Sort(List))
+
+# Best case Approach
+def Bubble_Sort(List):
+    for i in range(len(List)-1,0,-1):
+        Swapped=False
+        for j in range(i):
+            if List[j]>List[j+1]:
+                List[j],List[j+1] = List[j+1],List[j]
+                Swapped=True
+              
+        if not Swapped:
+            break
+        
+    print("Final Result:- ")
+    return List
+
+List1=[1,2,3,4,5]
+print(Bubble_Sort(List1))
